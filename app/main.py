b3f2fec23f83ca046e9ea9fb9c8be3617f6b12cc
Nikita <hkorobko@gmail.com>
2025-10-28 12:44:49 +0200

    Fix flake8

--- app/main.py
+++ app/main.py
@@ -9,16 +9,15 @@ class Person:
 
 def create_person_list(people: list[dict]) -> list["Person"]:
     Person.people.clear()
-    for data in people:
-        Person(data["name"], data["age"])
+    [Person(data["name"], data["age"]) for data in people]
 
     for data in people:
         person = Person.people[data["name"]]
 
-        if "wife" in data and data["wife"] is not None:
+        if data.get("wife") is not None:
             person.wife = Person.people[data["wife"]]
 
-        if "husband" in data and data["husband"] is not None:
+        if data.get("husband") is not None:
             person.husband = Person.people[data["husband"]]
 
     return list(Person.people.values())
